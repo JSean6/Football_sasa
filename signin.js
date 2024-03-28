521f075c4ce6a8914b11f789b66692f59e34227d
JSEAN6 <seannjoroge0@gmail.com>
2024-03-28 12:36:55 +0300

    add changes

--- signin.js
+++ signin.js
@@ -26,17 +26,16 @@
     <script src="signin.js"></script> */
   
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-app.js";
-  import { getAuth, GoogleAuthProvider } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
+  import { getAuth, GoogleAuthProvider, signInWithPopup } from "https://www.gstatic.com/firebasejs/10.9.0/firebase-auth.js";
 
 
   const firebaseConfig = {
-    apiKey: "AIzaSyCNX7LDLG8BQZuqIPsb_QBEBewM1OkmTmQ",
-    authDomain: "footballsasa-ab5e7.firebaseapp.com",
-    projectId: "footballsasa-ab5e7",
-    storageBucket: "footballsasa-ab5e7.appspot.com",
-    messagingSenderId: "634698697362",
-    appId: "1:634698697362:web:2402f5aa537e3ce1be344b",
-    measurementId: "G-ZLENCM2GK1"
+    apiKey: "AIzaSyDmT5gCl7O-8GzvF6ls9_7Lm-56UmEaaxA",
+    authDomain: "footballsasa-f5523.firebaseapp.com",
+    projectId: "footballsasa-f5523",
+    storageBucket: "footballsasa-f5523.appspot.com",
+    messagingSenderId: "109322016980",
+    appId: "1:109322016980:web:fc9f463631a1e28138c341"
   };
 
   
@@ -45,3 +44,37 @@
   auth.languageCode = 'en';
 
 const provider = new GoogleAuthProvider();
+
+
+
+const googleLogin = document.getElementById("google-login-btn");
+googleLogin.addEventListener("click", function() {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+      const credential = GoogleAuthProvider.credentialFromResult(result); 
+      const user = result.user;
+      console.log(user);
+      window.location.href = "../index.html";
+
+
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+    
+
+
+
+})
+
+function updateUserProfile(user) {
+    const userName = user.displayName;
+    const userEmail = user.email;
+    const userProfilePicture = user.photoURL;
+
+    document.getElementById("userName").textContext = userName;
+    document.getElementById("userEmail").textContext = userEmail;
+    document.getElementById("userProfilePicture").src = userProfilePicture;
+}
+
+updateUserProfile()
