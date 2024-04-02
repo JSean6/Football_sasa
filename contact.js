f5eb5517ca9825710ca82bfadf0d318509215fd7
JSEAN6 <seannjoroge0@gmail.com>
2024-04-02 08:35:58 +0300

    add changes

--- contact.js
+++ contact.js
@@ -1,27 +1,18 @@
-// const btn = document.getElementById('button');
-
-// document.getElementById('form')
-//  .addEventListener('submit', function(event) {
-//    event.preventDefault();
-
-//    btn.value = 'Sending...';
-
-//    const serviceID = 'default_service';
-//    const templateID = 'template_2ori7rc';
-
-//    emailjs.sendForm(serviceID, templateID, this)
-//     .then(() => {
-//       btn.value = 'Send Email';
-//       alert('Sent!');
-//     }, (err) => {
-//       btn.value = 'Send Email';
-//       alert(JSON.stringify(err));
-//     });
-// });
-
 const toggleButton = document.getElementsByClassName('toggle-btn')[0]
 const navBarLinks = document.getElementsByClassName('navbar-links')[0]
 
 toggleButton.addEventListener('click', () => {
   navBarLinks.classList.toggle('active')
 })
+
+function sendMail() {
+  var params = {
+    from_name1  : document.getElementById("fname").value,
+    from_name2  : document.getElementById("lname").value,
+    email_id : document.getElementById("email_id").value,
+    message : document.getElementById("message").value,
+  }
+  emailjs.send("service_6k7bfym", "template_2ori7rc", params).then(function (res) {
+    alert("success!" + res.status);
+  })
+}
