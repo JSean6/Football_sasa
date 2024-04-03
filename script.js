b043747c88f800c99ab2f015ddb2bc75b10bb4ec
JSEAN6 <seannjoroge0@gmail.com>
2024-04-03 11:25:43 +0300

    add changes

--- script.js
+++ script.js
@@ -13,12 +13,23 @@ async function fetchGuardianData(apiKey) {
     }
 }
 
+// const publicationDate = new Date(result.webPublicationDate);
+// const formattedDate = `${publicationDate.toLocaleDateString()} ${publicationDate.toLocaleTimeString()}`;
+// apiElement.innerHTML = `<strong>${result.sectionName}</strong>: ${result.webTitle} (Published: ${formattedDate})`;
+
 function renderApis(guardianData) {
     const apiContainer = document.getElementById('guardian');
     guardianData.forEach(result => {
+        const publicationDate = new Date(result.webPublicationDate);
+        const formattedDate = `${publicationDate.toLocaleDateString()} ${publicationDate.toLocaleTimeString()}`;
         const apiElement = document.createElement('div');
-        apiElement.innerHTML = `<div class="soccer"><strong style="color: fff">${result.sectionName}</strong>:  ${result.webTitle} <a href="${result.webUrl}">More on this</a> <p>${result.webPublicationDate}</p></div>`;
-        // const result.webPublicationDate = new Date(year,month,day,hours,minutes);
+        apiElement.innerHTML = `<div class="soccer"><strong style="color: fff">${result.sectionName}</strong>:  ${result.webTitle} 
+        <p>${formattedDate}</p>
+        
+        <button><a href="${result.webUrl}" style="text-decoration: none; color: white">More on this</a></button>
+        </div>`;
+        let webPublicationDate = new Date();
+        console.log(webPublicationDate.toLocaleDateString());
         // apiElement.innerHTML = `<div>
         //     <h3>${result.title}</h3>
         //     <p>By ${result.author.name}, Published on ${result.webPublicationDate.toLocaleString()}</p>
